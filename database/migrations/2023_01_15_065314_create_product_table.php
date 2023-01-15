95ad4b4793e185ff43a0f42ec49b6a5a0d612138
Andres Valle <andrex2d@gmail.com>
2023-01-15 02:16:25 -0500

    Estructura bd

---
+++ database/migrations/2023_01_15_065314_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->string('photo')->nullable();
+            $table->text('observation');
+            $table->enum('size', ['XS', 'S', 'M', 'L', 'XL']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
---
+++ database/migrations/2023_01_15_071306_create_relation_product_category_product.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelationProductCategoryProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->foreign('product_id')->references('id')->on('product')
+            ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
